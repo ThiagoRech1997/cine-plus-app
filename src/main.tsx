4744896bb44c1b85013f2fa694c4ac417ce31586
ThiagoRech1997 <thiagorech.1997@gmail.com>
2023-01-01 08:42:14 -0300

    Add React-Router-Dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
